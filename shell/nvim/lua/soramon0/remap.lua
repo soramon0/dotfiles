3a165013d4b649df2bee1e046da304239a815281
soramon0 <23435312+soramon0@users.noreply.github.com>
2023-07-23 15:52:09 +0100

    increate color column size, remove duplicate remap

--- shell/nvim/lua/soramon0/remap.lua
+++ shell/nvim/lua/soramon0/remap.lua
@@ -4,7 +4,6 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "<leader><leader>", function() vim.cmd("so") end)
 
 -- This is going to get me cancelled
