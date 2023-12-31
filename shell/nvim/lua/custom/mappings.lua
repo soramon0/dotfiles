a6c8e27ceddc01fc9e95e8946c6d5236d59bfe20
soramon0 <23435312+soramon0@users.noreply.github.com>
2023-12-31 15:38:24 +0100

    add debugger mappings

--- shell/nvim/lua/custom/mappings.lua
+++ shell/nvim/lua/custom/mappings.lua
@@ -1,64 +1,84 @@
 local M = {}
 
 M.dap = {
-  plugin = true,
-  n = {
-    ["<leader>db"] = {
-      "<cmd> DapToggleBreakpoint <CR>",
-      "Add breakpoint at line",
-    },
-    ["<leader>dus"] = {
-      function()
-        local widgets = require "dap.ui.widgets"
-        local sidebar = widgets.sidebar(widgets.scopes)
-        sidebar.open()
-      end,
-      "Open debugging sidebar",
-    },
-  },
+	plugin = true,
+	n = {
+		["<leader>db"] = {
+			"<cmd> DapToggleBreakpoint <CR>",
+			"Add breakpoint at line",
+		},
+		["<leader>dr"] = {
+			"<cmd> DapContinue<CR>",
+			"Run or continue debugger",
+		},
+		["<leader>dn"] = {
+			"<cmd> DapStepOver<CR>",
+			"Next line",
+		},
+		["<leader>di"] = {
+			"<cmd> DapStepInto<CR>",
+			"Step into method",
+		},
+		["<leader>do"] = {
+			"<cmd> DapStepOut<CR>",
+			"Step out of method",
+		},
+		["<leader>dx"] = {
+			"<cmd> DapTerminate<CR>",
+			"Terminate debugging session",
+		},
+		["<leader>dus"] = {
+			function()
+				local widgets = require("dap.ui.widgets")
+				local sidebar = widgets.sidebar(widgets.scopes)
+				sidebar.open()
+			end,
+			"Open debugging sidebar",
+		},
+	},
 }
 
 M.dap_go = {
-  plugin = true,
-  n = {
-    ["<leader>dgt"] = {
-      function()
-        require("dap-go").debug_test()
-      end,
-      "Debug go test",
-    },
-    ["<leader>dgl"] = {
-      function()
-        require("dap-go").debug_last()
-      end,
-      "Debug last go test",
-    },
-  },
+	plugin = true,
+	n = {
+		["<leader>dgt"] = {
+			function()
+				require("dap-go").debug_test()
+			end,
+			"Debug go test",
+		},
+		["<leader>dgl"] = {
+			function()
+				require("dap-go").debug_last()
+			end,
+			"Debug last go test",
+		},
+	},
 }
 
 M.gopher = {
-  plugin = true,
-  n = {
-    ["<leader>gsj"] = {
-      "<cmd> GoTagAdd json <CR>",
-      "Add json struct tags",
-    },
-    ["<leader>gsy"] = {
-      "<cmd> GoTagAdd yaml <CR>",
-      "Add yaml struct tags",
-    },
-  },
+	plugin = true,
+	n = {
+		["<leader>gsj"] = {
+			"<cmd> GoTagAdd json <CR>",
+			"Add json struct tags",
+		},
+		["<leader>gsy"] = {
+			"<cmd> GoTagAdd yaml <CR>",
+			"Add yaml struct tags",
+		},
+	},
 }
 
 M.crates = {
-  n = {
-    ["<leader>rcu"] = {
-      function()
-        require("crates").upgrade_all_crates()
-      end,
-      "Update rust crates",
-    },
-  },
+	n = {
+		["<leader>rcu"] = {
+			function()
+				require("crates").upgrade_all_crates()
+			end,
+			"Update rust crates",
+		},
+	},
 }
 
 return M
